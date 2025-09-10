1a3b56a03b7d8091fb9f9ebae9b248e8a736f610
duskt <30536860+Duskt@users.noreply.github.com>
2025-09-10 23:00:48 +0100

    use postgresjs

---
+++ server/src/db.ts
@@ -0,0 +1,12 @@
+import postgres from 'postgres'
+
+const database = process.env.TBSP_DBNAME || "TBSP";
+const sql = postgres({ database });
+
+export async function exampleQuery(age: number) {
+    const matches = await sql`
+	SELECT username FROM users WHERE age = ${ age }
+    `
+    console.log(matches);
+    return matches;
+}
