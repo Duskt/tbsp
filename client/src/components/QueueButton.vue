f4b313f6a8717bf7c2f71eaba67b7a4fb59cc836
duskt <30536860+Duskt@users.noreply.github.com>
2025-09-17 18:42:57 +0100

    add lobby page; remove example code

---
+++ client/src/components/QueueButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps<{ theme: String; }>();
+let playersQueued = ref(0);
+
+</script>
+
+<template>
+  <button @click="playersQueued++">
+    <span>{{ props.theme }}</span>
+    <p>Players Queued: {{playersQueued}}</p>
+  </button>
+</template>
+
+<style scoped>
+</style>
---
+++ client/src/views/Lobby.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import QueueButton from '../components/QueueButton.vue';
+// doesn't need to be dynamically updated
+const queues = ['Vanilla', 'Another'];
+</script>
+
+<template>
+  <main>
+    <table>
+      <tr v-for="q in queues">
+        <td>
+          <QueueButton :theme="q"/>
+        </td>
+      </tr>
+    </table>
+  </main>
+</template>
