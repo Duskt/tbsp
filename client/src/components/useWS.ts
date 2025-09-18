e3960c1cd1941788f44dbe46a3cba49cd746fe77
duskt <30536860+Duskt@users.noreply.github.com>
2025-09-18 17:09:45 +0100

    +useWS: composable which mounts a ws

---
+++ client/src/components/useWS.ts
@@ -0,0 +1,14 @@
+import { ref, onMounted, onUnmounted } from 'vue'
+
+/* FOR USE IN VIEWS - you don't want more than one WS connection per client! (?)
+ * Using this 'composable' function in a component will allow access
+ * to a WS connection which exists only when the component is rendered.
+ */
+export default function useWS(path: string = "/") {
+  const ws = ref<WebSocket>();
+
+  onMounted(() => ws.value = new WebSocket(path));
+  onUnmounted(() => { ws.value?.close(); ws.value = undefined})
+
+  return { ws }
+}
--- client/src/views/Lobby.vue
+++ client/src/views/Lobby.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue';
 import QueueButton from '../components/QueueButton.vue';
+import useWS from '../components/useWS.ts';
 import themes from '../../../src/theme.ts';
-// doesn't need to be dynamically updated
-const ws = new WebSocket('/');
+
+const { ws } = useWS('/');
+
 </script>
 
 <template>
