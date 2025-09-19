cd9a1406d45d51fccc95e91dc04c930530ee7d47
Liam Lethbridge <69471164+MrClever101@users.noreply.github.com>
2025-09-19 22:14:23 +0100

    messages save to database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,61 @@
 import { Elysia, file } from "elysia";
 import staticPlugin from "@elysiajs/static";
 import queueManager from "@server/queue";
-
+import postgres from 'postgres'
+import { createChatroomsTable, createMessageTable, createUserTable, createUsersInChatroomTable, createFakeData} from "./db";
+const sql = postgres({ 
+  database:"TBSP"
+ });
+createUserTable()
+createChatroomsTable()
+createMessageTable()
+createUsersInChatroomTable()
 const PORT = 9001;
 const CLIROOT = "../client/dist/";
 
 const app = new Elysia()
 
 .ws('/messages', {
-  message(ws, message) {
+   async message(ws, message) {
+    
     console.log('Received:', message)
-    ws.send(`Echo: ${message}`)
+
+    // temporary fixes: just setting IDs to 0
+    let chatRoomId = '00000000-0000-0000-0000-000000000001';
+    let userId = '00000000-0000-0000-0000-000000000001';
+   
+    try{
+    
+    await sql`
+    INSERT INTO messages (
+      chatroomid,
+      timestamp,
+      userid,
+      messagecontent
+    ) VALUES (
+      ${chatRoomId},
+      NOW(),
+      ${userId},
+      ${message}
+    )   
+    `;
+
+    let matches = await sql`SELECT * FROM messages`;
+    for (const row of matches){
+      console.log('Row keys:', Object.keys(row)); // See what columns you got
+      console.log('Content:', row.messagecontent); // Will work only if that key exists
+    }
+    }catch (error){
+      console.error('Failed to save message:', error);
+    }
+
   },
   open(ws) {
     console.log('Client connected')
   },
   close(ws) {
     console.log('Client disconnected')
+ 
   }
 })
 
