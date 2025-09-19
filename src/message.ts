297f6b7e83b3f826c551e95b8a315fd580a9bfbb
Liam Lethbridge <69471164+MrClever101@users.noreply.github.com>
2025-09-19 14:04:00 +0100

    added classes for messages and users

---
+++ src/message.ts
@@ -0,0 +1,17 @@
+import { User } from './user.ts'
+type ID = number;
+class Message {
+    userId: ID
+    body: string
+    chatroomId: ID
+    messageId: ID | null
+
+    constructor(userId: ID, body: string, chatroomId: ID, messageId: ID){
+        this.userId = userId;
+        this.body = body;
+        this.chatroomId = chatroomId;
+        this.messageId = messageId;
+    }
+}
+
+
---
+++ src/user.ts
@@ -0,0 +1,14 @@
+
+type ID = number;
+class User{
+    username: string;
+    passwordHash: string;
+    userId: ID;
+    games_played: number =0;
+    constructor(username: string, passwordHash: string, userId: ID, games_played: number = 0){
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.userId = userId;
+        this.games_played = games_played;
+    }
+}
