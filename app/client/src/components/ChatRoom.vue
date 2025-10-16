2c927b3ecfcfd2859713f960b64ba2296cee68e1
duskt <30536860+Duskt@users.noreply.github.com>
2025-10-16 17:23:32 +0100

    rebase

---
+++ app/client/src/components/ChatRoom.vue
@@ -0,0 +1,33 @@
+<script setup>
+import ChatBox from '../components/chat/ChatBox.vue';
+import ChatSelectionBar from '../components/chat/ChatSelectionBar.vue';
+import useWS from '../components/useWS.ts';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const { ws } = useWS('/messages', (msg) => {
+  if (!msg.data.startsWith('You')) {
+    console.warn('Unknown WS message:', msg.data);
+    return;
+  }
+  router.push('/chatroom');
+});
+
+console.log(ws);
+</script>
+
+<template>
+  <ChatRoomArea class="chat-room-area">
+    <ChatSelectionBar />
+    <ChatBox :ws="ws" />
+  </ChatRoomArea>
+</template>
+
+<style scoped>
+.chat-room-area {
+  display: flex;
+  height: 100vh;
+  width: 70vw;
+  flex-direction: column;
+}
+</style>
